bdccef52ee1b200df471b8f0c227753d647f5639
Emir Karamoldo <emirkaramoldo@proton.me>
2024-07-25 23:32:23 +0600

    В классе медик добавлены поле healPoint и добавлен метод increaseExperience

--- src/Medic.java
+++ src/Medic.java
@@ -1,5 +1,28 @@
 public class Medic extends Hero{
 
+    private int healPoints;
+
+    // Конструктор
+    public Medic(int healPoints) {
+        this.healPoints = healPoints;
+    }
+
+    // Геттер для healPoints
+    public int getHealPoints() {
+        return healPoints;
+    }
+
+    // Сеттер для healPoints
+    public void setHealPoints(int healPoints) {
+        this.healPoints = healPoints;
+    }
+
+    // Метод для увеличения опыта лечения
+    public void increaseExperience() {
+        healPoints = (int) (healPoints * 1.10); // Увеличиваем healPoints на 10%
+        System.out.println("Medic увеличил очки лечения до " + healPoints);
+    }
+
     @Override
     public void applySuperAbility() {
         setTypeSuperAbility("Омоложение");
